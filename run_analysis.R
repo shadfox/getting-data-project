2f31eac005177f069ecd2bf0f2e9ea99dae0d84e
Thiago Amanajas <ihgtao@gmail.com>
2016-06-26 16:11:37 +0100

    Fixing row

--- run_analysis.R
+++ run_analysis.R
@@ -1,9 +1,6 @@
 library(plyr)
 
 # Step 1
-# Merge the training and test sets to create one data set
-###############################################################################
-
 x_train <- read.table("UCI HAR Dataset/train/X_train.txt")
 y_train <- read.table("UCI HAR Dataset/train/y_train.txt")
 subject_train <- read.table("UCI HAR Dataset/train/subject_train.txt")
@@ -22,9 +19,6 @@ y_data <- rbind(y_train, y_test)
 subject_data <- rbind(subject_train, subject_test)
 
 # Step 2
-# Extract only the measurements on the mean and standard deviation for each measurement
-###############################################################################
-
 features <- read.table("UCI HAR Dataset/features.txt")
 
 # get only columns with mean() or std() in their names
@@ -37,9 +31,6 @@ x_data <- x_data[, mean_and_std_features]
 names(x_data) <- features[mean_and_std_features, 2]
 
 # Step 3
-# Use descriptive activity names to name the activities in the data set
-###############################################################################
-
 activities <- read.table("UCI HAR Dataset/activity_labels.txt")
 
 # update values with correct activity names
@@ -49,21 +40,12 @@ y_data[, 1] <- activities[y_data[, 1], 2]
 names(y_data) <- "activity"
 
 # Step 4
-# Appropriately label the data set with descriptive variable names
-###############################################################################
-
-# correct column name
 names(subject_data) <- "subject"
 
 # bind all the data in a single data set
 all_data <- cbind(x_data, y_data, subject_data)
 
 # Step 5
-# Create a second, independent tidy data set with the average of each variable
-# for each activity and each subject
-###############################################################################
-
-# 66 <- 68 columns but last two (activity & subject)
 averages_data <- ddply(all_data, .(subject, activity), function(x) colMeans(x[, 1:66]))
 
 write.table(averages_data, "averages_data.txt", row.name=FALSE)
